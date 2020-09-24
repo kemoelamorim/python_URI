4560de5664aec0dd3aa8b40b17706d81b9fbc31f
Kemoel <kemoel.amorim@gmail.com>
2020-09-24 13:33:49 -0300

    codigo_revisado

---
+++ exercicios_URI/URI_1013.py
@@ -0,0 +1,19 @@
+""" Faça um programa que leia três valores e apresente o maior dos três valores lidos seguido da mensagem “eh o maior”. Utilize a fórmula:
+
+maiorAB = (a + b + abs( a - b))/2
+
+Obs.: a fórmula apenas calcula o maior entre os dois primeiros (a e b). Um segundo passo, portanto é necessário para chegar no resultado esperado.
+
+Entrada
+O arquivo de entrada contém três valores inteiros.
+
+Saída
+Imprima o maior dos três valores seguido por um espaço e a mensagem "eh o maior".
+"""
+a, b, c = input().split(" ")
+a = int(a)
+b = int(b)
+c = int(c)
+maiorAB = (a + b + abs( a - b))/2
+maiorABC = int((maiorAB + c + abs(maiorAB - c))/2)
+print(maiorABC,"eh o maior")
